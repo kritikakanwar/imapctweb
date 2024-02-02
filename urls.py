2edfca63bdb3dca1b3812af0cb740edbec5bc9bb
Kritika Kanwar <kritikakanwar27@gmail.com>
2024-02-02 05:56:30 +0530

    added settings and url file

--- urls.py
+++ urls.py
@@ -15,22 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from .import views as v
-from django.conf import settings
-from django.conf.urls.static import static
-from .views import service
+from django.urls import path,include
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',v.home,name='Home'),
-    path('client/',v.client,name='Client'),
-    path('contact/',v.contactus,name='Contactus'),
-    path('vertical/',v.vertical,name='verticals'),
-    path('about/',v.about,name='About'),
-    path('service',v.service.as_view(),name='Service'),
-    
+    path('',include('impactapp.urls')),
 ]
-if settings.DEBUG:
-    urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
-if settings.DEBUG:
-    urlpatterns+=static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
